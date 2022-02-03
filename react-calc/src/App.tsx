97226f542ea4f5e892bfed60db834548398eac7e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-02-03 14:54:03 +0800

    fix startup error in react demo

--- react-calc/src/App.tsx
+++ react-calc/src/App.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  let [a, setA] = useState(1);
-  let [b, setB] = useState(1);
-  let [c, setC] = useState(a + b);
+  let [a, setA] = useState(1)
+  let [b, setB] = useState(1)
+  let [c, setC] = useState(a + b)
   useEffect(() => {
-    setC(a + b);
-  }, [a, b]);
+    setC(a + b)
+  }, [a, b])
   return (
     <>
       <input
         type="number"
         value={a}
-        onInput={(e) => setA((e.target as HTMLInputElement).valueAsNumber)}
+        onInput={e => setA(e.currentTarget.valueAsNumber)}
       />
       +
       <input
         type="number"
         value={b}
-        onInput={(e) => setB((e.target as HTMLInputElement).valueAsNumber)}
+        onInput={e => setB(e.currentTarget.valueAsNumber)}
       />
       =<span className="answer">{c}</span>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
